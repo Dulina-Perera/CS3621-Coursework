996c0e7afb9c18ecb7f1ba20d3fbf33ea7e11f5d
Dulina-Perera <dulina.perera@outlook.com>
2024-09-21 11:00:54 +0530

    _5_Intro-to-NoSQL-Databases/Lessons/Firestore/Cloud-Cafe: Retrieved documents from the Firestore.

--- _5_Intro-to-NoSQL-Databases/Lessons/Firestore/Cloud-Cafe/src/main.ts
+++ _5_Intro-to-NoSQL-Databases/Lessons/Firestore/Cloud-Cafe/src/main.ts
@@ -1,18 +1,43 @@
 import { FirebaseApp, initializeApp } from 'firebase/app';
-import { Firestore, getFirestore } from 'firebase/firestore';
+import { collection, Firestore, getDocs, getFirestore } from 'firebase/firestore';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+console.log(firebaseConfig);
 
 // Initialize Firebase.
 const app: FirebaseApp = initializeApp(firebaseConfig);
 
 // Initialize Firestore.
 const db: Firestore = getFirestore(app);
+
+// Display all cafes.
+const cafeList: Element | null = document.querySelector('#cafe-list');
+
+const renderCafe = (doc: any) => {
+	let li: HTMLElement = document.createElement('li');
+	let name: HTMLSpanElement = document.createElement('span');
+	let city: HTMLSpanElement = document.createElement('span');
+
+	li.setAttribute('data-id', doc.id);
+	name.textContent = doc.data().name;
+	city.textContent = doc.data().city;
+
+	li.appendChild(name);
+	li.appendChild(city);
+
+	cafeList?.appendChild(li);
+};
+
+getDocs(collection(db, 'cafes')).then((snapshot) => {
+	snapshot.docs.forEach((doc) => {
+		renderCafe(doc);
+	});
+});
